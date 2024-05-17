837354ae389e6b8f3f8ed06d1f04a320ac019839
rakibulhasan044 <rhrakib044@gmail.com>
2024-05-17 18:15:08 +0600

    category filter added

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 6001;
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erh7g8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,39 +24,54 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
-    const roomsCollection = client.db('splendico').collection('rooms');
+    const roomsCollection = client.db("splendico").collection("rooms");
 
-    app.get('/rooms', async (req, res) => {
-        try {
-            const result = await roomsCollection.find().toArray();
-            res.send(result);
-        } catch (error) {
-            console.error("Error fetching rooms:", error);
-            res.status(500).json({ error: "Internal server error" });
-        }
+    app.get("/rooms", async (req, res) => {
+      try {
+        console.log(req.query);
+        const page = parseInt(req.query.page) -1;
+        const size = parseInt(req.query.size);
+        const filter = req.query.filter;
+
+        let query = {}
+        if(filter) query = { category: filter}
+
+        const result = await roomsCollection
+        .find(query)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
 
-    app.get('/roomsCount', async(req, res) => {
-      const count = await roomsCollection.estimatedDocumentCount();
-      res.send({count});
-    })
-    
-    app.get('/rooms/:category', async (req, res) => {
-        try {
-            const category = req.params.category;
-            const query = { category: category };
-            const result = await roomsCollection.find(query).toArray();
-            res.send(result);
-        } catch (error) {
-            console.error("Error fetching rooms by category:", error);
-            res.status(500).json({ error: "Internal server error" });
-        }
+    app.get("/roomsCount", async (req, res) => {
+      const filter = req.query.filter;
+      let query = {};
+      if(filter) query ={ category: filter }
+      const count = await roomsCollection.countDocuments(query);
+      res.send({ count });
     });
-    
-    
+
+    app.get("/rooms/:category", async (req, res) => {
+      try {
+        const category = req.params.category;
+        const query = { category: category };
+        const result = await roomsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching rooms by category:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -67,11 +79,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) =>{
-    res.send('Slendico hotel server running')
-})
+app.get("/", (req, res) => {
+  res.send("Slendico hotel server running");
+});
 
 app.listen(port, () => {
-    console.log(`Slendico hotel server running on port: ${port}`);
-})
+  console.log(`Slendico hotel server running on port: ${port}`);
+});
